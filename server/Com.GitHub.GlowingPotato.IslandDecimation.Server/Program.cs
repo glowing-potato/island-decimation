a6afdb3f903f1b78458e6c103e310b29f0c5bbea
Zach Deibert <zachdeibert@gmail.com>
2017-11-03 23:01:40 -0500

    Changed project to run on .NET Framework

--- server/Com.GitHub.GlowingPotato.IslandDecimation.Server/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Com.GitHub.GlowingPotato.IslandDecimation.Server {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
---
+++ server/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Com.GitHub.PotatoEarth.IslandDecimation.Server {
+    public class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
