5e6b4873e9e1f9bdfae61e98d4e3801991af7828
Hanavan99 <hanavan99@gmail.com>
2017-11-04 01:51:58 -0500

    Fixed Brian's fail

--- client/src/main/java/com/github/glowingpotato/islanddecimation/Main.java
+++ client/src/main/java/com/github/glowingpotato/islanddecimation/Main.java
@@ -3,8 +3,7 @@ package com.github.glowingpotato.islanddecimation;
 import com.github.glowingpotato.islanddecimation.gui.LogInScreen;
 
 public class Main {
-    public static void main(String[] args0)
-    {
-	new LogInScreen();
-    }
+	public static void main(String[] args0) {
+		new LogInScreen();
+	}
 }
