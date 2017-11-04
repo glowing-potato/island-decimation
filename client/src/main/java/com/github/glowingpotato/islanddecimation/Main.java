b8b018bbe8d31272ec6ade0f035a423fae2ff6ef
Hanavan99 <hanavan99@gmail.com>
2017-11-04 03:00:05 -0500

    Somebody messed up the build.gradle (Brian)

--- client/src/main/java/com/github/glowingpotato/islanddecimation/Main.java
+++ client/src/main/java/com/github/glowingpotato/islanddecimation/Main.java
@@ -1,10 +1,17 @@
 package com.github.glowingpotato.islanddecimation;
 
+import org.lwjgl.glfw.GLFW;
+
+import com.github.glowingpotato.islanddecimation.gui.GLWindow;
 import com.github.glowingpotato.islanddecimation.gui.LogInScreen;
 
 public class Main {
 	public static void main(String[] args0) {
 		LogInScreen screen = new LogInScreen();
-		
+		GLFW.glfwInit();
+		GLWindow window = new GLWindow(800, 600, "Testing", 0);
+		window.makeContextCurrent();
+		window.init();
+		window.loop();
 	}
 }
