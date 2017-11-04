38efa086cb7613bf2bdb08f73f7a9df3993fab96
Zach Deibert <zachdeibert@gmail.com>
2017-11-03 22:26:22 -0500

    Created server project

---
+++ server/Com.GitHub.GlowingPotato.IslandDecimation.Server/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Com.GitHub.GlowingPotato.IslandDecimation.Server {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
