a5877b8181af6d2f7596a2b0f15cdb967fcc40a6
Hanavan99 <hanavan99@gmail.com>
2017-11-04 14:54:56 -0500

    Added some test models and made a car draw on the screen.

--- client/src/main/java/com/github/glowingpotato/islanddecimation/Main.java
+++ client/src/main/java/com/github/glowingpotato/islanddecimation/Main.java
@@ -1,5 +1,6 @@
 package com.github.glowingpotato.islanddecimation;
 
+import java.io.File;
 import java.util.Arrays;
 
 import javax.swing.JOptionPane;
@@ -7,26 +8,33 @@ import javax.swing.JOptionPane;
 import org.lwjgl.glfw.GLFW;
 
 import com.github.glowingpotato.islanddecimation.gui.GLWindow;
+import com.github.glowingpotato.islanddecimation.render.GUIRenderer;
 import com.github.glowingpotato.islanddecimation.render.IslandRenderer;
 import com.github.glowingpotato.islanddecimation.render.SkyRenderer;
 import com.github.glowingpotato.islanddecimation.state.GameState;
 import com.github.glowingpotato.islanddecimation.util.Perlin;
 import com.github.glowingpotato.islanddecimation.view.Island;
+import com.github.glowingpotato.islanddecimation.view.Resource;
 import com.github.glowingpotato.islanddecimation.view.Terrain;
+import com.glutilities.text.FontManager;
 
 public class Main {
 	public static void main(String[] args) {
-	    if (args.length != 2) {
-            /*
-             * For debugging, place your email as the first parameter, and your password as the second parameter in the
-             * run configuration for Eclipse (it's under Run Configurations > Arguments > Program Arguments).
-             */
-	        JOptionPane.showMessageDialog(null, "Please run this program through the launcher instead of manually.");
-	        System.exit(1);
-	    }
-	    String username = args[0];
-	    String password = args[1];
+		if (args.length != 2) {
+			/*
+			 * For debugging, place your email as the first parameter, and your password as
+			 * the second parameter in the run configuration for Eclipse (it's under Run
+			 * Configurations > Arguments > Program Arguments).
+			 */
+			JOptionPane.showMessageDialog(null, "Please run this program through the launcher instead of manually.");
+			System.exit(1);
+		}
+		String username = args[0];
+		String password = args[1];
+		
 		GLFW.glfwInit();
+		File f = new File("src/main/resources/models/car.obj");
+		GameState.getState().getModelManager().load(f, "test");
 		GLWindow window = new GLWindow(800, 600, "Testing", 0);
 		window.makeContextCurrent();
 		window.init();
@@ -37,12 +45,13 @@ public class Main {
 			int y = i / IslandRenderer.ISLAND_SIZE - IslandRenderer.ISLAND_RADIUS;
 			float dist = (float) Math.pow(Math.sqrt(x * x + y * y), 2) / 1200f;
 			shape[i] = 10f - dist
-					- (float) p.noise(i % IslandRenderer.ISLAND_SIZE / 64d, i / IslandRenderer.ISLAND_SIZE / 64d, 0) * 3
-							;
+					- (float) p.noise(i % IslandRenderer.ISLAND_SIZE / 64d, i / IslandRenderer.ISLAND_SIZE / 64d, 0)
+							* 3;
 		}
 		Terrain t = new Terrain(Arrays.asList(shape));
-		GameState.getState().getIslands().add(new Island(0.0, null, null, t, null));
+		GameState.getState().getIslands().add(new Island(0.0, null, new Resource(1, 2, 3), t, null));
 		window.addUIRenderer(new SkyRenderer());
+		//window.addUIRenderer(new GUIRenderer());
 		window.addGameRenderer(new IslandRenderer());
 		window.loop();
 		window.close();
---
+++ client/src/main/java/com/github/glowingpotato/islanddecimation/render/GUIRenderer.java
@@ -0,0 +1,25 @@
+package com.github.glowingpotato.islanddecimation.render;
+
+import java.util.List;
+
+import com.github.glowingpotato.islanddecimation.state.GameState;
+import com.github.glowingpotato.islanddecimation.view.Island;
+import com.github.glowingpotato.islanddecimation.view.Resource;
+import com.glutilities.text.FontManager;
+
+public class GUIRenderer extends Renderer {
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+		List<Island> islands = GameState.getState().getIslands();
+		System.out.println(islands);
+		Island i = islands.get(0);
+		System.out.println(i);
+		Resource r = i.getResources();
+		System.out.println(r);
+		int iridium = r.getIridium();
+		FontManager.drawString("Arial", "test"/*String.valueOf(iridium)*/, 0.1, 0.1, 0.1, 16d / 9);
+	}
+
+}
