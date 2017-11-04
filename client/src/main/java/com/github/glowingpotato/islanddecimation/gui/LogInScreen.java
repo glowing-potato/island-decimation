7fd2779e667e3bb6081f9cf993495ecf75ebe152
Nathan Gawith <NathanGawith@gmail.com>
2017-11-04 04:10:57 -0500

    Made the log in screen look even prettier, but not as pretty as what Zach is about to commit

--- client/src/main/java/com/github/glowingpotato/islanddecimation/gui/LogInScreen.java
+++ client/src/main/java/com/github/glowingpotato/islanddecimation/gui/LogInScreen.java
@@ -1,9 +1,14 @@
 package com.github.glowingpotato.islanddecimation.gui;
 
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.SpringLayout;
 
@@ -11,9 +16,13 @@ public class LogInScreen {
 
 	private JLabel label = new JLabel("Login");
 	private JTextField usernameTextField = new JTextField();
-	private JTextField passwordTextField = new JTextField();
+	private JPasswordField passwordTextField = new JPasswordField();
 	private JLabel userNameLabel = new JLabel("Username:");
 	private JLabel passwordLabel = new JLabel("Password:");
+	private JButton logInButton = new JButton("Log In");
+	
+	private int frameWidth = 300;
+	private int frameHeight = 200;
 	
 	public LogInScreen() {
 		JFrame frame = new JFrame();
@@ -31,11 +40,20 @@ public class LogInScreen {
 		panel.add(passwordTextField);
 		panel.add(userNameLabel);
 		panel.add(passwordLabel);
+		panel.add(logInButton);
 		panel.add(label);
-		frame.setSize(500, 250);
+		frame.setSize(frameWidth, frameHeight);
 		update(panel);
-		frame.setSize(501, 250);
-		update(panel);
+//		frame.setSize(501, 250);
+//		update(panel);
+ 		logInButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent action) {
+				username = usernameTextField.getText();
+				password = new String(passwordTextField.getPassword());
+				System.out.println(password);
+				frame.dispose();
+			}
+		});
 	}
 	
 	private void update(JPanel panel) {
@@ -43,20 +61,23 @@ public class LogInScreen {
 		SpringLayout springLayout = new SpringLayout();
 		panel.setLayout(springLayout);
 		
-//		springLayout.putConstraint(SpringLayout.NORTH, label, panel.getHeight() / 5, SpringLayout.NORTH, panel);
-//		springLayout.putConstraint(SpringLayout.EAST, label, panel.getWidth() / 2, SpringLayout.EAST, panel);
+		springLayout.putConstraint(SpringLayout.NORTH, label, 0, SpringLayout.NORTH, panel);
+		springLayout.putConstraint(SpringLayout.WEST, label, frameWidth / 2, SpringLayout.WEST, panel);
 		
-		springLayout.putConstraint(SpringLayout.NORTH, usernameTextField, 104, SpringLayout.NORTH, panel);
-		springLayout.putConstraint(SpringLayout.WEST, usernameTextField, 182, SpringLayout.WEST, panel);
+		springLayout.putConstraint(SpringLayout.NORTH, usernameTextField, frameHeight / 3, SpringLayout.NORTH, panel);
+		springLayout.putConstraint(SpringLayout.EAST, usernameTextField, -10, SpringLayout.EAST, panel);
 		
-		springLayout.putConstraint(SpringLayout.NORTH, passwordTextField, 21, SpringLayout.SOUTH, usernameTextField);
+		springLayout.putConstraint(SpringLayout.NORTH, passwordTextField, 10, SpringLayout.SOUTH, usernameTextField);
 		springLayout.putConstraint(SpringLayout.EAST, passwordTextField, 0, SpringLayout.EAST, usernameTextField);
 		
 		springLayout.putConstraint(SpringLayout.NORTH, userNameLabel, 0, SpringLayout.NORTH, usernameTextField);
 		springLayout.putConstraint(SpringLayout.EAST, userNameLabel, -45, SpringLayout.WEST, usernameTextField);
-		
+
 		springLayout.putConstraint(SpringLayout.NORTH, passwordLabel, 0, SpringLayout.NORTH, passwordTextField);
 		springLayout.putConstraint(SpringLayout.EAST, passwordLabel, 0, SpringLayout.EAST, userNameLabel);
+		
+		springLayout.putConstraint(SpringLayout.SOUTH, logInButton, 0, SpringLayout.SOUTH, panel);
+		springLayout.putConstraint(SpringLayout.EAST, logInButton, 0, SpringLayout.EAST, panel);
 	}
 	
 
@@ -64,22 +85,10 @@ public class LogInScreen {
 	private String password = "";
 
 	public String getUsername() {
-		username = usernameTextField.getText();
 		return username;
 	}
 
 	public String getPassword() {
-		password = passwordTextField.getText();
 		return password;
 	}
-	
-	
-// 		logIn.addActionListener(new ActionListener() {
-// 			public void actionPerformed(ActionEvent action) {
-// 				username = usernameField.getText();
-// 				password = new String(passwordField.getPassword());
-// 				System.out.println(password);
-// 				frame.dispose();
-// 			}
-// 		});
 }
